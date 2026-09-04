82f26e862f35c77dfc36cfa307e08807fe7ce055
azan-personal <112457939+azanra@users.noreply.github.com>
2026-09-04 09:33:23 +0700

    refactor: remove boilerplate code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
 import "./styles.css";
-import { greeting } from "./greeting.ts";
-import dummyImg from "./dummy.jpg";
 
-console.log(greeting);
-
-const image = document.createElement("img");
-image.src = dummyImg;
-
-document.body.appendChild(image);
+console.log("Hello World");
